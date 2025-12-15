ef551876b1c622ad2607912c2102c05f605dc042
DevOpsTerraformLabs <ghanshammahajan29@gmail.com>
2025-12-15 19:08:50 +0530

    Azure NSG Association

--- example/simple/main.tf
+++ example/simple/main.tf
@@ -1,13 +1,13 @@
 // example/simple/main.tf
 //module "nsg_association" {
 //  source     = "../../" # root of repo
-//  nsg_id     = "/subscriptions/9d85477c-d2a6-46aa-9b01-fed6058bf542/resourceGroups/rg-example-remote/providers/Microsoft.Network/networkSecurityGroups/example-nsg-baseline"
-//  subnet_ids = ["/subscriptions/9d85477c-d2a6-46aa-9b01-fed6058bf542/resourceGroups/rg-example-remote/providers/Microsoft.Network/virtualNetworks/example-vnet-remote/subnets/app"]
+//  nsg_id     = "/subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/resourceGroups/rg-example-remote/providers/Microsoft.Network/networkSecurityGroups/example-nsg-baseline"
+//  subnet_ids = ["/subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/resourceGroups/rg-example-remote/providers/Microsoft.Network/virtualNetworks/example-vnet-remote/subnets/app"]
 //}
 
 //Remote
 module "nsg_association" {
   source     = "git@github.com:DevOpsTerraformLabs/terraform-azurerm-nsg-association.git" # root of repo
-  nsg_id     = "/subscriptions/9d85477c-d2a6-46aa-9b01-fed6058bf542/resourceGroups/rg-example-remote/providers/Microsoft.Network/networkSecurityGroups/example-nsg-baseline"
-  subnet_ids = ["/subscriptions/9d85477c-d2a6-46aa-9b01-fed6058bf542/resourceGroups/rg-example-remote/providers/Microsoft.Network/virtualNetworks/example-vnet-remote/subnets/app"]
+  nsg_id     = "/subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/resourceGroups/rg-example-remote/providers/Microsoft.Network/networkSecurityGroups/example-nsg-baseline"
+  subnet_ids = ["/subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/resourceGroups/rg-example-remote/providers/Microsoft.Network/virtualNetworks/example-vnet-remote/subnets/app"]
 }
